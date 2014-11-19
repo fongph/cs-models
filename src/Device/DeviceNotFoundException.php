493cee029b7955dc3bef5c46d33e637c1147a038
Orest Budzhak <doom4eg@gmail.com>
2014-11-19 17:41:59 +0200

    fix device record load exception

---
+++ src/Device/DeviceNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CS\Models\Device;
+
+/**
+ * Description of DeviceNotFoundException
+ *
+ * @author root
+ */
+class DeviceNotFoundException extends \Exception
+{
+    //put your code here
+}
